fa50feb330e84768001f34477a50b7888333b73e
wendy03 <wendysu03@gmail.com>
2023-05-01 14:55:42 +0800

    fix: 刪除多於 require

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { isAuth, generateSendJWT } = require('../services/auth');
+const { isAuth } = require('../services/auth');
 const UserController = require('../controllers/user');
 
 router.get('/account', isAuth, UserController.getUser);
