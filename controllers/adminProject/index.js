4327f67e41c5ad23e9792c6e3704cfc155ea7060
Chi-Ling-Lee <19940818jillian@gmail.com>
2023-05-10 16:29:55 +0800

    feat: [BF-3]get project detail (read project)

--- controllers/adminProject/index.js
+++ controllers/adminProject/index.js
@@ -1,5 +1,7 @@
 const handleCreateProject = require('./createProject');
+const handleReadProject = require('./readProject');
 const handleReadTeam = require('./readTeam');
 
 module.exports.handleCreateProject = handleCreateProject;
+module.exports.handleReadProject = handleReadProject;
 module.exports.handleReadTeam = handleReadTeam;
--- routes/adminProject.js
+++ routes/adminProject.js
@@ -6,6 +6,7 @@ const { isAuth, generateSendJWT, isAdmin } = require('../services/auth');
 const ProjectController = require('../controllers/adminProject');
 
 router.post('/', isAdmin, ProjectController.handleCreateProject);
+router.get('/:id/info', isAdmin, ProjectController.handleReadProject);
 
 //teams
 router.get('/:projId/team/:teamId', isAdmin, ProjectController.handleReadTeam);
