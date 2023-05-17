ebbe74804332256dedd6f3df9c0dd9bed4ae039a
Chi-Ling-Lee <19940818jillian@gmail.com>
2023-05-17 22:04:14 +0800

    fix: [FF-1] add pagination

--- controllers/project/getProjectList.js
+++ controllers/project/getProjectList.js
@@ -6,7 +6,7 @@ const handleErrorAsync = require('../../services/handleErrorAsync');
 const Project = require('../../models/projectsModel');
 
 const handleGetProjectList = handleErrorAsync(async (req, res, next) => {
-  const { sort, category } = req.query;
+  const { sort, category, page = '1', limit = '10' } = req.query;
   let projList = await Project.find({ isAbled: 1 }).populate({
     path: 'teamId',
     select: 'title'
@@ -21,11 +21,15 @@ const handleGetProjectList = handleErrorAsync(async (req, res, next) => {
       'project_backers'
     ])
   ) {
-    return next(appError(400, '路由資訊錯誤'));
+    return next(appError(400, '路由資訊錯誤，sort 資料錯誤'));
   }
 
   if (category && !validator.isIn(category, ['0', '1', '2'])) {
-    return next(appError(400, '路由資訊錯誤'));
+    return next(appError(400, '路由資訊錯誤，category 資料錯誤'));
+  }
+
+  if (!validator.isInt(page) || !validator.isInt(limit)) {
+    return next(appError(400, '路由資訊錯誤，page 或 limit 資料錯誤'));
   }
 
   //最新上線
@@ -64,7 +68,22 @@ const handleGetProjectList = handleErrorAsync(async (req, res, next) => {
     projList = projList.filter((x) => x.category === 2);
   }
 
-  successHandler(res, '取得專案列表成功', projList);
+  const pageN = Number(page);
+  const limitN = Number(limit);
+  const items = projList.slice((pageN - 1) * limitN, pageN * limitN);
+  const total = projList.length;
+  const totalPages = Math.ceil(total / limitN);
+
+  const result = {
+    total,
+    totalPages,
+    items,
+    startIndex: (pageN - 1) * limitN + 1,
+    endIndex: (pageN - 1) * limitN + items.length,
+    limit: limitN
+  };
+
+  successHandler(res, '取得專案列表成功', result);
 });
 
 module.exports = handleGetProjectList;
