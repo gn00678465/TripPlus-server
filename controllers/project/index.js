16d3e4233d424911b421f3fd4242bb51cad41e8a
Chi-Ling-Lee <19940818jillian@gmail.com>
2023-05-18 17:09:16 +0800

    feat: [FF-5] get project faqs

--- controllers/project/index.js
+++ controllers/project/index.js
@@ -1,7 +1,9 @@
 const handleGetProjectList = require('./getProjectList');
 const handleGetProject = require('./getProject');
 const handleGetProjectNews = require('./getProjectNews');
+const handleGetProjectFaqs = require('./getProjectFaqs');
 
 module.exports.handleGetProjectList = handleGetProjectList;
 module.exports.handleGetProject = handleGetProject;
 module.exports.handleGetProjectNews = handleGetProjectNews;
+module.exports.handleGetProjectFaqs = handleGetProjectFaqs;
--- routes/project.js
+++ routes/project.js
@@ -5,5 +5,6 @@ const ProjectController = require('../controllers/project');
 router.get('/', ProjectController.handleGetProjectList);
 router.get('/:id', ProjectController.handleGetProject);
 router.get('/:id/news', ProjectController.handleGetProjectNews);
+router.get('/:id/faqs', ProjectController.handleGetProjectFaqs);
 
 module.exports = router;
