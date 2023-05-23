ba6ce3ce463939a1499759ac620b95f3d88ea418
wendy03 <wendysu03@gmail.com>
2023-05-23 15:13:27 +0800

    fix: 欄位空值比對

--- socket/index.js
+++ socket/index.js
@@ -1,4 +1,5 @@
 const Message = require('../models/messagesModel');
+const appError = require('../services/appError');
 
 // 建立 WebSocket 連接
 module.exports = (io) => {
@@ -7,13 +8,23 @@ module.exports = (io) => {
 
     socket.on('message', async (messagePayload) => {
       const { sender, receiver, content, projectId } = messagePayload;
-      const message = await Message.create({
-        sender,
-        receiver,
-        content,
-        projectId
-      });
-      io.emit('message', { ...messagePayload });
+      if (!sender || !receiver || !content || !projectId) {
+        return io.emit(
+          'error',
+          '以下欄位不可爲空：接收者 id、聊天訊息、專案 id 不可為空'
+        );
+      }
+      try {
+        const message = await Message.create({
+          sender,
+          receiver,
+          content,
+          projectId
+        });
+        io.emit('message', { ...messagePayload });
+      } catch (error) {
+        console.log(error);
+      }
     });
 
     socket.on('disconnect', () => {
