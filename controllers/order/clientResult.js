51eeb4fb1a26261396b14ffd18c2b930caaf67d0
Chi-Ling-Lee <19940818jillian@gmail.com>
2023-06-08 00:30:18 +0800

    fix: res end

--- controllers/order/clientResult.js
+++ controllers/order/clientResult.js
@@ -8,9 +8,11 @@ const handleErrorAsync = require('../../services/handleErrorAsync');
 const handleClientResult = handleErrorAsync((req, res, next) => {
   const { RtnCode } = req.body;
   if (RtnCode == 1) {
-    res.redirect(process.env.PaymentSuccessURL).end();
+    res.redirect(process.env.PaymentSuccessURL);
+    res.end();
   } else {
-    res.redirect(process.env.PaymentFailedURL).end();
+    res.redirect(process.env.PaymentFailedURL);
+    res.end();
   }
 });
 
