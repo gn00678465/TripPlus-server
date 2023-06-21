4f8cc6c6fa5eb02debfdccb1d53fbad0bbea4036
Wendy03 <wendysu03@gmail.com>
2023-06-21 12:49:54 +0800

    fix: 修正 get user chatroom router

--- routes/message.js
+++ routes/message.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { isAuth, isAdmin } = require('../services/auth');
 const messagesController = require('../controllers/message');
 
-router.get('/chatroom', isAuth, messagesController.getUserChatroom);
+router.get('/user/chatroom', isAuth, messagesController.getUserChatroom);
 router.get(
   '/admin/:projectId/chatroom',
   isAdmin,
